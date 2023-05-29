3d771e5eb9b2f5e7190abef9cea1fc5d2501b218
kyw0300 <kyw0300@naver.com>
2023-05-30 02:18:46 +0900

    230530 p.123까지 기본 설정 및 JUnit 테스트 코드 작성법, 간단한 controller 작성

---
+++ ex00/src/main/java/org/zerock/web/SampleController2.java
@@ -0,0 +1,22 @@
+package org.zerock.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller
+public class SampleController2 {
+
+	private static final Logger logger = 
+		LoggerFactory.getLogger(SampleController2.class);
+	
+	@GetMapping("/doC")
+	public String doC(@ModelAttribute("msg") String msg) {
+		logger.info("doC() 실행...!");
+		
+		return "result";
+	}
+	
+}
